810602c7b455f2e8d1f99d05ea454b711a0a5666
mrsajadpp <mrsajadpp@gmail.com>
2025-03-27 14:48:56 +0530

    fix: update login route path for consistency in API structure

--- route/user/post/auth/login.js
+++ route/user/post/auth/login.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../../../../models/User');
 
-router.post('/login', async (req, res) => {
+router.post('/', async (req, res) => {
     const { email, password } = req.body;
 
     try {
